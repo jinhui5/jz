9c39cb6466833c236306794c28d08481775db298
jinhui5 <haoniu9368@gmail.com>
2025-07-05 19:31:22 +0800

    Update handler.py

--- bot/handler.py
+++ bot/handler.py
@@ -17,3 +17,5 @@ def setup_handlers(application):
     # 注册 "删除操作人" 文本输入
     application.add_handler(MessageHandler(filters.TEXT & filters.Regex(r'删除操作人'), remove_operator))
 
+    # 注册 "设置操作人" 文本输入
+    application.add_handler(MessageHandler(filters.TEXT & filters.Regex(r'设置操作人'), set_operator))
