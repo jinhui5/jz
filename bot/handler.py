9440f02b09717a9e27fca365ae01c6cdd58b2d69
jinhui5 <haoniu9368@gmail.com>
2025-07-05 19:52:56 +0800

    Update handler.py

--- bot/handler.py
+++ bot/handler.py
@@ -1,5 +1,5 @@
 from telegram.ext import CommandHandler, MessageHandler, filters
-from .commands import set_operator, remove_operator, show_operators
+from .commands import set_operator, remove_operator, show_operators, show_exchange_rate
 
 def setup_handlers(application):
     # 注册 /set_operator 命令
@@ -10,6 +10,9 @@ def setup_handlers(application):
 
     # 注册 /show_operators 命令
     application.add_handler(CommandHandler("show_operators", show_operators))
+
+    # 注册 /show_exchange_rate 命令
+    application.add_handler(CommandHandler("show_exchange_rate", show_exchange_rate))
     
     # 注册 "设置操作人" 文本输入
     application.add_handler(MessageHandler(filters.TEXT & filters.Regex(r'设置操作人'), set_operator))
@@ -19,3 +22,6 @@ def setup_handlers(application):
 
     # 注册 "显示操作人" 文本输入
     application.add_handler(MessageHandler(filters.TEXT & filters.Regex(r'显示操作人'), show_operators))
+
+    # 注册 "显示实时汇率" 文本输入
+    application.add_handler(MessageHandler(filters.TEXT & filters.Regex(r'显示实时汇率'), show_exchange_rate))
