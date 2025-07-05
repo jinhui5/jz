6332a06342d99d55b5b3a38bc3b4d2bc203f4e9b
jinhui5 <haoniu9368@gmail.com>
2025-07-05 18:59:13 +0800

    Update handler.py

--- bot/handler.py
+++ bot/handler.py
@@ -1,20 +1,6 @@
-from telegram.ext import CommandHandler, MessageHandler, filters  # 使用 filters
-from .commands import start_set_operator, set_operator
+from telegram.ext import CommandHandler, MessageHandler, filters
+from .commands import set_operator
 
 def setup_handlers(application):
-    # 使用 filters.Text 和 filters.Regex 来组合过滤条件
-    application.add_handler(MessageHandler(filters.TEXT, start_set_operator))
-    application.add_handler(MessageHandler(filters.Regex(r'设置操作人'), start_set_operator))
-
-    # 使用 ConversationHandler 来管理对话流
-    conv_handler = ConversationHandler(
-        entry_points=[MessageHandler(filters.Text & filters.Regex(r'设置操作人'), start_set_operator)],
-        states={
-            # 用户输入用户名的状态
-            1: [MessageHandler(filters.Text & ~filters.Command(), set_operator)],
-        },
-        fallbacks=[],
-    )
-
-    # 注册处理器
-    application.add_handler(conv_handler)
+    # 注册处理 "设置操作人" 文本输入
+    application.add_handler(MessageHandler(filters.TEXT & filters.Regex(r'设置操作人'), set_operator))
