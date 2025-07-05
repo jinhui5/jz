1d5473bdbc2d37c6244b46fbe4573ca2f97878af
jinhui5 <haoniu9368@gmail.com>
2025-07-05 18:21:04 +0800

    Update handler.py

--- bot/handler.py
+++ bot/handler.py
@@ -1,7 +1,6 @@
 from telegram.ext import CommandHandler
-from .commands import set_operator, add_ledger, get_ledger
+from .commands import set_operator
 
 def setup_handlers(application):
+    # 注册 set_operator 命令
     application.add_handler(CommandHandler("set_operator", set_operator))
-    application.add_handler(CommandHandler("add", add_ledger))
-    application.add_handler(CommandHandler("ledger", get_ledger))
