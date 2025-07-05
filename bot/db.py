1567a79b9806eb31daf9d01cdfbc5bb131af6d1e
jinhui5 <haoniu9368@gmail.com>
2025-07-05 18:06:22 +0800

    Update db.py

--- bot/db.py
+++ bot/db.py
@@ -1,1 +1,15 @@
+import os
+import psycopg2
+from urllib.parse import urlparse
+
+def get_db_connection():
+    url = urlparse(os.environ["DATABASE_URL"])
+    conn = psycopg2.connect(
+        database=url.path[1:],
+        user=url.username,
+        password=url.password,
+        host=url.hostname,
+        port=url.port
+    )
+    return conn
 
