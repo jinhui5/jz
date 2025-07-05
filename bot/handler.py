4ee34444dc7c3fcf6ba7778888274dd9ee2de9a8
jinhui5 <haoniu9368@gmail.com>
2025-07-05 20:32:54 +0800

    Update handler.py

--- bot/handler.py
+++ bot/handler.py
@@ -1,5 +1,5 @@
 from telegram.ext import CommandHandler, MessageHandler, filters
-from .commands import set_operator, remove_operator, show_operators, show_exchange_rate, set_exchange_rate, deposit_rmb, spend_rmb, deposit_usdt, spend_usdt
+from .commands import set_operator, remove_operator, show_operators, show_exchange_rate, set_exchange_rate, deposit_rmb, spend_rmb, deposit_usdt, spend_usdt, show_daily_bill
 
 def setup_handlers(application):
     # 注册 /set_operator 命令
@@ -16,6 +16,9 @@ def setup_handlers(application):
 
     # 注册 /set_exchange_rate 命令
     application.add_handler(CommandHandler("set_exchange_rate", set_exchange_rate))
+
+    # 注册 /show_daily_bill 命令（显示账单）
+    application.add_handler(CommandHandler("show_daily_bill", show_daily_bill))
 
     # 注册 "入款人民币" 命令 (+数字c)
     application.add_handler(MessageHandler(filters.TEXT & filters.Regex(r'^\+\d+\.?\d*c$'), deposit_rmb))
@@ -43,4 +46,7 @@ def setup_handlers(application):
 
     # 注册 "设置实时汇率" 文本输入
     application.add_handler(MessageHandler(filters.TEXT & filters.Regex(r'设置实时汇率'), set_exchange_rate))
+
+    # 注册 "显示账单" 文本输入
+    application.add_handler(MessageHandler(filters.TEXT & filters.Regex(r'显示账单'), show_daily_bill))
 
