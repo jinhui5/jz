0d74b39bcc4e6e1fb3b74aca9b5810139078165b
jinhui5 <haoniu9368@gmail.com>
2025-07-05 19:24:45 +0800

    Update handler.py

--- bot/handler.py
+++ bot/handler.py
@@ -7,9 +7,15 @@ def setup_handlers(application):
     
     # 注册 /remove_operator 命令
     application.add_handler(CommandHandler("remove_operator", remove_operator))
+
+    # 注册 /show_operators 命令
+    application.add_handler(CommandHandler("show_operators", show_operators))
     
     # 注册 "设置操作人" 文本输入
     application.add_handler(MessageHandler(filters.TEXT & filters.Regex(r'设置操作人'), set_operator))
 
     # 注册 "删除操作人" 文本输入
     application.add_handler(MessageHandler(filters.TEXT & filters.Regex(r'删除操作人'), remove_operator))
+
+    # 注册 "显示操作人" 文本输入
+    application.add_handler(MessageHandler(filters.TEXT & filters.Regex(r'显示操作人'), show_operators))
