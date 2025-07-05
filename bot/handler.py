7a7fd9df3634fbc2f1687d9acf785bd1f6933b25
jinhui5 <haoniu9368@gmail.com>
2025-07-05 19:02:01 +0800

    Update handler.py

--- bot/handler.py
+++ bot/handler.py
@@ -1,6 +1,6 @@
-from telegram.ext import CommandHandler, MessageHandler, filters
+from telegram.ext import MessageHandler, filters
 from .commands import set_operator
 
 def setup_handlers(application):
-    # 注册处理 "设置操作人" 文本输入
+    # 注册 "设置操作人" 文本输入
     application.add_handler(MessageHandler(filters.TEXT & filters.Regex(r'设置操作人'), set_operator))
